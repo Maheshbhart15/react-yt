8548d0e7905b9eff1df2a8a286b6413ef99689d7
Maheshbhart15 <127493536+Maheshbhart15@users.noreply.github.com>
2024-08-01 15:34:33 +0530

    Vite

---
+++ Vite-React/Vite-helloWorld/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <h1>Trupss Attending the It shaala npx degit user/project#main my-project
+cd my-project
+
+npm install
+npm run dev</h1>
+  )
+}
+
+export default App
